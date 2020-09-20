305d5e75c9ef3bfcdfdc8b0a11c9bf9fa43877b1
Gabriel Roriz <rorizcontact@gmail.com>
2020-09-19 23:26:03 -0300

    Master

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
-THEME_NAME="Nome do Tema"
-THEME_AUTHOR="Gabriel Roriz"
+THEME_NAME="Insigne"
+THEME_AUTHOR="Gabriel Roriz e Matheus Cândido"
 THEME_AUTHOR_URI="https://www.gabrielroriz.com.br"
 THEME_DESCRIPTION=""
 THEME_VERSION="0.1"
