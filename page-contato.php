6aced9f810a7a39fc5540c7313f1bc1f3b17af34
Matheus Candido <matheusoc@live.com>
2020-09-22 22:17:40 -0300

    Página de contato Desktop

---
+++ page-contato.php
@@ -0,0 +1,66 @@
+<?php get_header() ?>
+
+<div class="contato__header">
+    <div class="contato__header__wrapper">
+
+        <h1>
+            Entre <br/>
+            <span>em</span><br/>
+            contato
+        </h1>
+
+    </div>
+</div>
+
+<div class="contato__infos">
+    <div class="contato__infos__wrapper">
+        <div></div>
+
+
+        <div class="contato__infos__infos">
+
+
+            <div class="contato__infos__row">
+                <h4>Endereço</h4>
+                <h2>
+                    Av. Professor Alfonso Bovero, 1057.
+                </h2>
+            </div>
+
+
+            <div class="contato__infos__row">
+                <h4>Telefone</h4>
+                <h2>
+                    +55 (11) 4502-4718
+                </h2>
+            </div>
+
+
+            <div class="contato__infos__row">
+                <h4>Redes sociais</h4>
+                <div class="contato__infos__social">
+                    <a href="#">Instagram</a>
+                    <a href="#">Facebook</a>
+                    <a href="#">YouTube</a>
+                </div>
+            </div>
+
+            <div class="contato__infos__row">
+                <div class="contato__infos__wpp">
+                    <a href="#">
+                        Whatsapp
+                    </a>    
+                </div>
+                <div class="contato__infos__smalltext">
+                    Horário de atendimento:<br/>
+                    das 08:00h às 17:00h
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+</div>
+
+<?php get_footer() ?>
