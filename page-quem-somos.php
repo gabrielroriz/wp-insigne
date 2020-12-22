612b9a2ccdd06f35c33da6eb809df9cd56835861
Matheus Candido <matheusoc@live.com>
2020-12-21 21:49:36 -0300

    Início da página sobre

---
+++ page-quem-somos.php
@@ -0,0 +1,61 @@
+<?php get_header() ?>
+<div class="sobre">
+
+    <div class="sobre__navbar"></div>
+
+    <div class="sobre__header">
+    </div>
+
+    <div class="sobre__texto1">
+        <div class="sobre__texto1__left">
+            <div class="sobre__texto1__move">
+                É isso que nos move
+            </div>
+            <div>
+                Somos uma produtora audiovisual <b>que te leva aonde você merece estar.</b>
+            </div>
+            <div>
+                Somos para aqueles que só se <b>contentam com o melhor.</b>
+            </div>
+        </div>
+        <div class="sobre__texto1__right">
+            <div>Entregamos soluções audiovisuais criativas em diversos formatos: storytelling, lançamento em super lives, animação, branded content, institucionais, programas para internet e tv, e todo tipo de obra original.</div>
+            <div><b>Somos um time de criativos e técnicos a serviço de quem quer alcançar o sucesso do jeito certo. Sabemos o que fazer e como fazer.</b></div>
+            <div>Mais que vídeos, fazemos arte cinematográfica.</div>
+            <div><b>Somos Insigne.</b></div>
+        </div>
+    </div>
+    <div class="sobre__texto1-bottom">
+    </div>
+
+
+    <div class="sobre__beliefs">
+        <div class="sobre__beliefs__nisto">
+            E é nisso que acreditamos
+        </div>
+
+        <div class="sobre__beliefs__belief sobre__beliefs__belief__b1 ">
+            <div class="sobre__beliefs__belief__numeral">
+                01
+            </div>
+            <div class="sobre__beliefs__belief__text">
+                Acreditamos que a palavra e um aperto de mão são mais fortes que qualquer assinatura em um pedaço de papel.
+            </div>
+        </div>
+
+        <div class="sobre__beliefs__belief sobre__beliefs__belief__b2">
+            <div class="sobre__beliefs__belief__numeral">
+                01
+            </div>
+        </div>
+
+        <div class="sobre__beliefs__belief sobre__beliefs__belief__b3">
+            <div class="sobre__beliefs__belief__numeral">
+                01
+            </div>
+        </div>
+    </div>
+    
+    
+</div>
+<?php // get_footer() ?>
