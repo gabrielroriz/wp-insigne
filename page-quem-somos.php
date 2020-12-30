8ea7815be0ced9bf2e42cda2392609f67e6cdd8a
Matheus Candido <matheusoc@live.com>
2020-12-30 15:09:10 -0300

    Ajuste do header da página sobre desktop

--- page-quem-somos.php
+++ page-quem-somos.php
@@ -4,6 +4,7 @@
     <div class="sobre__navbar"></div>
 
     <div class="sobre__header">
+        <img width="100%" src="<?php echo get_template_directory_uri() . "/assets/min-images/quemsomos/img-cavalo-header.png"; ?>" alt="">
     </div>
 
     <div class="sobre__texto1">
@@ -56,6 +57,5 @@
         </div>
     </div>
     
-    
+    <?php get_footer() ?>
 </div>
-<?php // get_footer() ?>
