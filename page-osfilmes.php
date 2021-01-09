54a8111403c19ae104794c0d9a2d5cef476cb925
Matheus Candido <matheusoc@live.com>
2021-01-08 23:44:41 -0300

    Início da tela Filmes

--- page-osfilmes.php
+++ page-osfilmes.php
@@ -1,49 +1,74 @@
-<?php get_header() ?>
+<div class="filmes">
+    <?php get_header() ?>
 
-<div class="filmes__header">
-    <div class="filmes__header__wrapper">
-        
+    <div class="filmes__header">
         <div class="filmes__header__title">
             <h1>
                 Nossos filmes
             </h1>
             <h4>
-                Subtítulo explicativo referente à página e ao quê o usuário encontrará.
+                Aqui estão as nossas artes cinematográficas.
             </h4>
         </div>
 
         <div class="filmes__header__posters">
-            <img class="filmes__poster filmes__poster__1" src="https://via.placeholder.com/520x763" alt="Pôster 1">
-            <img class="filmes__poster filmes__poster__2" src="https://via.placeholder.com/520x763" alt="Pôster 1">
-            <img class="filmes__poster filmes__poster__3" src="https://via.placeholder.com/520x763" alt="Pôster 1">
-            <img class="filmes__poster filmes__poster__fundo" src="https://via.placeholder.com/520x763" alt="">
+            <div class="filmes__highlight" style="background-image: url('<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>');">
+                <div class="filmes__highlight__number">
+                    01
+                </div>
+                <div class="filmes__highlight__text">
+                    General Electric
+                </div>
+            </div>
         </div>
     </div>
-</div>
 
-<div class="filmes__tabs">
-    <div class="filmes__tabs__wrapper">
-        <a href="#" class="filmes__tabs__button filmes__tabs__selected" id="tab_todos_button">
-            Todos os trabalhos
-        </a>
+    <div class="filmes__destaques">
 
-        <a href="#" class="filmes__tabs__button" id="tab_tipo_button">
-            Tipo de trabalho
-        </a>
-    </div>
-</div>
-
-<div class="filmes__tab" id="tab_todos">
-    <?php for($i = 0; $i < 20; $i++): ?>
-        <div class="filmes__poster">
+        <div class="filmes__destaques__title">
+            <div class="filmes__destaques__text">Destaques</div>
+            <div class="filmes__destaques__img filmes__destaques__img1">
+                <img src="<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>" />
+            </div>
+            <div class="filmes__destaques__img filmes__destaques__img2">
+                <img src="<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>" />
+            </div>
+            <div class="filmes__destaques__img filmes__destaques__img3">
+                <img src="<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>" />
+            </div>
+            <div class="filmes__destaques__img filmes__destaques__img4">
+                <img src="<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>" />
+            </div>
+            <div class="filmes__destaques__img filmes__destaques__img5">
+                <img src="<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>" />
+            </div>
         </div>
-    <?php endfor; ?>
-</div>
 
-<div class="filmes__tab" id="tab_tipo">
-    <div class="filmes__tab__wrapper">
+    </div>
 
-        <?php 
+    <div class="filmes__tabs">
+        <div class="filmes__tabs__wrapper">
+            <a href="#" class="filmes__tabs__button filmes__tabs__selected" id="tab_todos_button">
+                Todos os trabalhos
+            </a>
+
+            <a href="#" class="filmes__tabs__button" id="tab_tipo_button">
+                Tipo de trabalho
+            </a>
+        </div>
+    </div>
+
+    <div class="filmes__tab" id="tab_todos">
+        <?php for ($i = 0; $i < 20; $i++) : ?>
+            <div class="filmes__poster">
+            </div>
+        <?php endfor; ?>
+    </div>
+
+    <div class="filmes__tab" id="tab_tipo">
+        <div class="filmes__tab__wrapper">
+
+            <?php
             $tipos = [
                 "Storytelling",
                 "Lançamentos - Super Lives",
@@ -65,11 +90,11 @@
             //     ]
             // ]);
 
-        ?>
-        
-        <?php foreach($categorias as $categoria): ?>
+            ?>
 
-            <?php 
+            <?php foreach ($categorias as $categoria) : ?>
+
+                <?php
                 $filmes = new WP_Query([
                     "post_type" => "filmes",
                     "tax_query" => [
@@ -80,28 +105,28 @@
                         ]
                     ]
                 ]);
-            ?>
+                ?>
 
-            <div class="filmes__row">
-                <div class="filmes__row__head">
-                    <h3><?php echo $categoria->name; ?></h3>
-                    <h4><a href="<?php echo "/filmes-category/" . $categoria->slug . "/"; ?>">Veja todos</a></h4>
+                <div class="filmes__row">
+                    <div class="filmes__row__head">
+                        <h3><?php echo $categoria->name; ?></h3>
+                        <h4><a href="<?php echo "/filmes-category/" . $categoria->slug . "/"; ?>">Veja todos</a></h4>
+                    </div>
+                    <div class="filmes__row__grid">
+                        <?php while ($filmes->have_posts()) : $filmes->the_post(); ?>
+                            <div class="filmes__griditem">
+                                <img src="<?php echo get_post_meta(get_the_ID(), "FILMES_IMAGEM")[0]; ?>">
+                            </div>
+                        <?php endwhile; ?>
+                    </div>
                 </div>
-                <div class="filmes__row__grid">
-                    <?php while($filmes->have_posts()): $filmes->the_post(); ?>
-                        <div class="filmes__griditem">
-                            <img src="<?php echo get_post_meta(get_the_ID(), "FILMES_IMAGEM")[0]; ?>">
-                        </div>  
-                    <?php endwhile; ?>
-                </div>
-            </div>
 
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+
+        </div>
 
     </div>
+
+    <?php //get_footer()
+    ?>
 </div>
-
-</div>
-
-
-<?php get_footer()?>
