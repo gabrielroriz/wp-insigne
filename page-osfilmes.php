281e6208c55d3834507f9cfcf410922a6345a92f
Matheus Candido <matheusoc@live.com>
2021-01-18 22:49:21 -0300

    Adições da página filmes

--- page-osfilmes.php
+++ page-osfilmes.php
@@ -27,24 +27,77 @@
 
         <div class="filmes__destaques__title">
             <div class="filmes__destaques__text">Destaques</div>
-            <div class="filmes__destaques__img filmes__destaques__img1">
-                <img src="<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>" />
+            <div class="filmes__destaques__img filmes__destaques__img1" style="background-image: url('<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>')">
+                <div class="filmes__destaques__img__overlay">
+                    <div class="filmes__destaques__img__overlaytext">
+                        <h2>General Electric</h2>
+                        <h4>General Electric</h4>
+                    </div>
+                </div>
             </div>
-            <div class="filmes__destaques__img filmes__destaques__img2">
-                <img src="<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>" />
+            <div class="filmes__destaques__img filmes__destaques__img2" style="background-image: url('<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>')">
+                <div class="filmes__destaques__img__overlay">
+                    <div class="filmes__destaques__img__overlaytext">
+                        <h2>General Electric</h2>
+                        <h4>General Electric</h4>
+                    </div>
+                </div>
             </div>
-            <div class="filmes__destaques__img filmes__destaques__img3">
-                <img src="<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>" />
+            <div class="filmes__destaques__img filmes__destaques__img3" style="background-image: url('<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>')">
+                <div class="filmes__destaques__img__overlay">
+                    <div class="filmes__destaques__img__overlaytext">
+                        <h2>General Electric</h2>
+                        <h4>General Electric</h4>
+                    </div>
+                </div>
             </div>
-            <div class="filmes__destaques__img filmes__destaques__img4">
-                <img src="<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>" />
+            <div class="filmes__destaques__img filmes__destaques__img4" style="background-image: url('<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>')">
+                <div class="filmes__destaques__img__overlay">
+                    <div class="filmes__destaques__img__overlaytext">
+                        <h2>General Electric</h2>
+                        <h4>General Electric</h4>
+                    </div>
+                </div>
             </div>
-            <div class="filmes__destaques__img filmes__destaques__img5">
-                <img src="<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>" />
+            <div class="filmes__destaques__img filmes__destaques__img5" style="background-image: url('<?php echo get_template_directory_uri() . "/assets/min-images/filmes/img-primeiro-projeto.png"; ?>')">
+                <div class="filmes__destaques__img__overlay">
+                    <div class="filmes__destaques__img__overlaytext">
+                        <h2>General Electric</h2>
+                        <h4>General Electric</h4>
+                    </div>
+                </div>
             </div>
         </div>
 
     </div>
+
+    <script>
+        function todosFilmesOnClick(event) {
+            event.preventDefault();
+
+            document.getElementById("tab_tipos").style.opacity = 0;
+            document.getElementById("tab_tipos").style.height = 0;
+
+            document.getElementById("tab_todos").style.opacity = 1;
+            document.getElementById("tab_todos").style.height = "auto";
+
+            document.getElementById("tab_tipo_button").classList.remove("filmes__tabs__selected");
+            document.getElementById("tab_todos_button").classList.add("filmes__tabs__selected");
+        }
+
+        function tipoDeTrabalhoOnClick(event) {
+            event.preventDefault();
+
+            document.getElementById("tab_todos").style.opacity = 0;
+            document.getElementById("tab_todos").style.height = 0;
+
+            document.getElementById("tab_tipos").style.opacity = 1;
+            document.getElementById("tab_tipos").style.height = "auto";
+
+            document.getElementById("tab_todos_button").classList.remove("filmes__tabs__selected");
+            document.getElementById("tab_tipo_button").classList.add("filmes__tabs__selected");
+        }
+    </script>
 
     <div class="filmes__tabs">
         <div class="filmes__tabs__title">
@@ -53,17 +106,26 @@
             </div>
         </div>
         <div class="filmes__tabs__wrapper">
-            <a href="#" class="filmes__tabs__button filmes__tabs__selected" id="tab_todos_button">
+            <a onclick="todosFilmesOnClick(event)" href="#" class="filmes__tabs__button filmes__tabs__selected" id="tab_todos_button">
                 Todos os filmes
             </a>
 
-            <a href="#" class="filmes__tabs__button" id="tab_tipo_button">
+            <a onclick="tipoDeTrabalhoOnClick(event)" href="#" class="filmes__tabs__button" id="tab_tipo_button">
                 Tipo de trabalho
             </a>
         </div>
         <div class="filmes__tab" id="tab_todos">
-            <?php for ($i = 0; $i < 20; $i++) : ?>
-                <div class="filmes__tab__poster" style="background-image: url('<?php echo get_template_directory_uri() . "/assets/min-images/filmes/poster.png"; ?>');">
+
+            <?php
+                $filmes = new WP_Query([
+                    "post_type" => "filmes",
+                    "posts_per_page" => 16,
+                    "page" => 1
+                ]);
+            ?>
+        
+            <?php while ($filmes->have_posts()) : $filmes->the_post(); ?>
+                <div class="filmes__tab__poster" style="background-image: url('<?php echo get_post_meta(get_the_ID(), "FILMES_IMAGEM")[0]; ?>');">
                     <div class="filmes__tab__poster__grid">
                         <div class="filmes__tab__poster__text">
                             <span>
@@ -72,67 +134,67 @@
                         </div>
                     </div>
                 </div>
-            <?php endfor; ?>
+            <?php endwhile; ?>
         </div>
-    </div>
 
-    <div class="filmes__tab" id="tab_tipo">
-        <div class="filmes__tab__wrapper">
-
-            <?php
-            $tipos = [
-                "Storytelling",
-                "Lançamentos - Super Lives",
-                "Publicidade",
-                "Animação/Motion",
-                "Obras Originais",
-                "Branded Content",
-                "Institucionais"
-            ];
-            $categorias = get_terms([
-                "taxonomy" => "filmes-category",
-                "hide_empty" => false
-            ]);
-
-            // $filmes_por_categoria = new WP_Query([
-            //     "post_type"=> "filmes",
-            //     "tax_query" => [
-            //         "taxonomy" => "filmes-category"
-            //     ]
-            // ]);
-
-            ?>
-
-            <?php foreach ($categorias as $categoria) : ?>
+        <div class="filmes__tab" id="tab_tipos">
+            <div class="filmes__tab__wrapper">
 
                 <?php
-                $filmes = new WP_Query([
-                    "post_type" => "filmes",
-                    "tax_query" => [
-                        [
-                            "taxonomy" => "filmes-category",
-                            "field" => "slug",
-                            "terms" => $categoria->slug
-                        ]
-                    ]
+                $tipos = [
+                    "Storytelling",
+                    "Lançamentos - Super Lives",
+                    "Publicidade",
+                    "Animação/Motion",
+                    "Obras Originais",
+                    "Branded Content",
+                    "Institucionais"
+                ];
+                $categorias = get_terms([
+                    "taxonomy" => "filmes-category",
+                    "hide_empty" => false
                 ]);
+
                 ?>
 
-                <div class="filmes__row">
-                    <div class="filmes__row__head">
-                        <h3><?php echo $categoria->name; ?></h3>
-                        <h4><a href="<?php echo "/filmes-category/" . $categoria->slug . "/"; ?>">Veja todos</a></h4>
-                    </div>
-                    <div class="filmes__row__grid">
-                        <?php while ($filmes->have_posts()) : $filmes->the_post(); ?>
-                            <div class="filmes__griditem">
-                                <img src="<?php echo get_post_meta(get_the_ID(), "FILMES_IMAGEM")[0]; ?>">
-                            </div>
-                        <?php endwhile; ?>
-                    </div>
-                </div>
+                <?php foreach ($categorias as $categoria) : ?>
 
-            <?php endforeach; ?>
+                    <?php
+                    $filmes = new WP_Query([
+                        "post_type" => "filmes",
+                        "tax_query" => [
+                            [
+                                "taxonomy" => "filmes-category",
+                                "field" => "slug",
+                                "terms" => $categoria->slug
+                            ]
+                        ]
+                    ]);
+                    ?>
+
+                    <div class="filmes__row">
+                        <div class="filmes__row__head">
+                            <h3><?php echo $categoria->name; ?></h3>
+                            <h4><a href="<?php echo "/filmes-category/" . $categoria->slug . "/"; ?>">Veja todos</a></h4>
+                        </div>
+                        <div class="filmes__row__grid">
+                            <?php while ($filmes->have_posts()) : $filmes->the_post(); ?>
+                            <div class="filmes__tab__poster" style="background-image: url('<?php echo get_post_meta(get_the_ID(), "FILMES_IMAGEM")[0]; ?>');">
+                                <div class="filmes__tab__poster__grid">
+                                    <div class="filmes__tab__poster__text">
+                                        <span>
+                                            Catho
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php endwhile; ?>
+                        </div>
+                    </div>
+
+                <?php endforeach; ?>
+
+            </div>
 
         </div>
 
@@ -146,7 +208,7 @@
             </div>
             <div class="filmes__cta__button">
                 <a href="">Entrar em contato via WhatsApp</a>
-                <span>Horário de atendimento: <br/> das 08:00h às 17:00h.</span>
+                <span>Horário de atendimento: <br /> das 08:00h às 17:00h.</span>
             </div>
         </div>
         <img width="100%" src="<?php echo get_template_directory_uri() . "/assets/min-images/quemsomos/notavel-baixo.svg"; ?>" alt="">
